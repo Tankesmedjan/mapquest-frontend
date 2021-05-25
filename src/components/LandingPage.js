548adc96a818d78dcedc4519b5831fe440a3f421
ddewe <ddewee92@gmail.com>
2021-05-25 14:01:38 +0200

    animated logo

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,13 +6,13 @@ export class LandingPage extends Component {
     render() {
         return (
                 <div className="container">
-                    <p><button className="btn flashy-btn"><Icon.CurrencyEuro /> Purchase Access</button></p>
+                    <p><button className="btn flashy-btn"><Icon.CurrencyDollar /> Purchase Access</button></p>
                     <p><button className="btn flashy-btn"><Icon.DoorOpen /> Login</button></p>
                     <p><button className="btn flashy-btn"><Icon.Upc /> Scan QR</button></p>
                     <p><button className="btn flashy-btn"><Icon.BoxArrowInRight /> Quick access</button></p>
 
-                    <div className="logo-img">
-                        <img src="/images/mapquest-logo.png" alt="logo" width="200"/>
+                    <div>
+                        <img className="logo-img" src="/images/mapquest-logo.png" alt="logo" width="200"/>
                     </div>
                 </div>
         )
