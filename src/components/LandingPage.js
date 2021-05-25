c1fb267b05b23b373d2c4b8822ae00ba745308b4
Michael <michael@graphicdesire.se>
2021-05-25 17:42:02 +0200

    wrapper added, css fixed for landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
 import * as Icon from "react-bootstrap-icons";
 import {Link} from "react-router-dom";
 
@@ -7,18 +7,25 @@ export class LandingPage extends Component {
     render() {
         return (
                 <div className="container">
+                    <div className="wrapper-main">
+                        <h2 className="maps-header" style={{color: '#61dafb', marginBottom: '-10px'}}>Welcome to MapQuest</h2>
+                        <b>The best geo based game online, for a fun and active game play!<br/>
+                        Build your own story with missions from our library or chose from many ready-to-go stories. </b><br/>
+                            <small><i>Only $24.99 for 24h access to the created game.</i></small><br/><br/>
 
-                    <p><button className="btn flashy-btn"><Icon.CurrencyDollar /> Purchase Access</button></p>
-                    <p><Link to="/login"><button className="btn flashy-btn"><Icon.DoorOpen /> Login</button></Link></p>
-                    <p><button className="btn flashy-btn"><Icon.Upc /> Scan QR</button></p>
-                    <p><Link to="/googlemap"><button className="btn flashy-btn"><Icon.BoxArrowInRight /> Quick access</button></Link></p>
-
-                    <div className="logo-holder">
-                        <img className="logo-img" src="/images/mapquest-logo.png" alt="logo" width="200"/><br/>
-                       &reg;&copy; 2021 - Tankesmedjan. <a href="mailto:tankesmedjan@protonmail.com">Mail us</a>
+                        <p><button className="btn flashy-btn"><Icon.CurrencyDollar /> Purchase Access</button></p>
+                        <p><Link to="/login"><button className="btn flashy-btn"><Icon.DoorOpen /> Login</button></Link></p>
+                        <p><button className="btn flashy-btn"><Icon.Upc /> Scan QR</button></p>
+                        <p><Link to="/googlemap"><button className="btn flashy-btn"><Icon.BoxArrowInRight /> Quick access</button></Link></p>
                     </div>
-                    <img className="skyline-image" src="/images/gothenburg-skyline.png" alt="skyline-image" width="440"/>
+                    <div className="wrapper-footer">
+                        <div className="logo-holder">
+                            <img className="logo-img" src="/images/mapquest-logo.png" alt="logo" width="200"/><br/>
+                           &reg;&copy; 2021 - Tankesmedjan. <a href="mailto:tankesmedjan@protonmail.com">Mail us</a>
+                        </div>
+                        <img className="skyline-image" src="/images/gothenburg-skyline.png" alt="skyline-image" width="440"/>
                     </div>
+                </div>
 
         )
     }
--- src/components/Login.js
+++ src/components/Login.js
@@ -14,30 +14,31 @@ export class Login extends Component {
 
     render() {
         return (
-            <>
-
             <div className="container">
-                <h1 className="maps-header">Login</h1>
-                <form onSubmit={this.submitLogin}>
-                    <div className="form-group">
-                        <input type="text" placeholder="Enter your username" className="form-control"
-                               name="username" onChange={this.handleInputChange}/>
-                    </div><br/>
-                    <div className="form-group">
-                        <input placeholder="Enter your password" type="password" className="form-control"
-                               name="password" onChange={this.handleInputChange}/>
-                    </div><br/>
-                    <a href="#!">I Lost My Password</a><br/><br/>
-                    <button type="submit" className="btn flashy-btn">Login</button>
-                </form>
-
-                <div className="logo-holder">
-                    <img className="logo-img" src="/images/mapquest-logo.png" alt="logo" width="200"/><br/>
-                    &reg;&copy; 2021 - Tankesmedjan. <a href="mailto:tankesmedjan@protonmail.com">Mail us</a>
+                <div className="wrapper-main">
+                    <h2 className="maps-header" style={{color: '#61dafb', marginBottom: '-10px'}}>Welcome to MapQuest</h2>
+                    <h3 className="maps-header">Login</h3>
+                    <form onSubmit={this.submitLogin}>
+                        <div className="form-group">
+                            <input type="text" placeholder="Enter your username" className="form-control"
+                                   name="username" onChange={this.handleInputChange}/>
+                        </div><br/>
+                        <div className="form-group">
+                            <input placeholder="Enter your password" type="password" className="form-control"
+                                   name="password" onChange={this.handleInputChange}/>
+                        </div><br/>
+                        <a href="#!">I Lost My Password</a><br/><br/>
+                        <button type="submit" className="btn flashy-btn">Login</button>
+                    </form>
                 </div>
-                <img className="skyline-image" src="/images/gothenburg-skyline.png" alt="skyline-image" width="440"/>
+                <div className="wrapper-footer">
+                    <div className="logo-holder">
+                        <img className="logo-img" src="/images/mapquest-logo.png" alt="logo" width="200"/><br/>
+                        &reg;&copy; 2021 - Tankesmedjan. <a href="mailto:tankesmedjan@protonmail.com">Mail us</a>
+                    </div>
+                    <img className="skyline-image" src="/images/gothenburg-skyline.png" alt="skyline-image" width="440"/>
+                </div>
             </div>
-                </>
         )
     }
 }
