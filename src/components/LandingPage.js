79335aec3b631129f1eecf901a8ae44cc4ecd4ab
MichMatch <michael@graphicdesire.se>
2021-05-25 13:39:09 +0200

    buttons designed

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,10 +6,10 @@ export class LandingPage extends Component {
     render() {
         return (
                 <div className="container">
-                    <p><button className="btn btn-light"><Icon.CurrencyEuro /> Purchase Access</button></p>
-                    <p><button className="btn btn-light"><Icon.DoorOpen /> Login</button></p>
-                    <p><button className="btn btn-light"><Icon.Upc /> Scan QR</button></p>
-                    <p><button className="btn btn-light"><Icon.BoxArrowInRight /> Quick access</button></p>
+                    <p><button className="btn flashy-btn"><Icon.CurrencyEuro /> Purchase Access</button></p>
+                    <p><button className="btn flashy-btn"><Icon.DoorOpen /> Login</button></p>
+                    <p><button className="btn flashy-btn"><Icon.Upc /> Scan QR</button></p>
+                    <p><button className="btn flashy-btn"><Icon.BoxArrowInRight /> Quick access</button></p>
                 </div>
         )
     }
