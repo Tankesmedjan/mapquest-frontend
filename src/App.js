525a9b59bd0729441b793b5ff485d9efbe212d52
MichMatch <michael@graphicdesire.se>
2021-05-25 10:44:50 +0200

    resize of map, new header for App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
-      <h1>MapQuest</h1>
+      <h1 className="maps-header">MapQuest</h1>
       <GoogleMap />
     </div>
   );
