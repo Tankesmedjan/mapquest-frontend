68cca8ed99cc67b9674185cba373b2c12f54ae8f
MichMatch <michael@graphicdesire.se>
2021-05-26 14:11:06 +0200

    ScanQR updates

--- src/components/ScanQR.js
+++ src/components/ScanQR.js
@@ -1,14 +1,47 @@
 import React, {Component} from "react";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as Icon from "react-bootstrap-icons";
+import QrReader from 'react-qr-scanner';
 
 export class ScanQR extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            delay: 100,
+            result: 'No result',
+        }
+
+        this.handleScan = this.handleScan.bind(this)
+    }
+
+    handleScan(data){
+        this.setState({
+            result: data,
+        })
+    }
+    handleError(err){
+        console.error(err)
+    }
 
     render() {
+        const previewStyle = {
+            height: 280,
+            width: 280,
+            facingMode: "rear",
+            chooseDeviceId: 1,
+        }
         return (
             <div className="container">
                 <div className="wrapper-main">
                     <h2 className="maps-header" style={{color: '#61dafb', marginBottom: '-10px'}}>Welcome to MapQuest</h2>
-                    <h3 className="maps-header">The Eminent QR Scanner</h3>
+                    <QrReader
+                        delay={this.state.delay}
+                        style={previewStyle}
+                        onError={this.handleError}
+                        onScan={this.handleScan}
+                    />
+                    <input type="text" placeholder="Enter QR code here: " name="QRcode" style={{padding: '5px', width: '200px' }} /><button className="btn" type="submit"><Icon.BoxArrowInRight /></button>
+
                 </div>
                 <div className="wrapper-footer">
                     <div className="logo-holder">
