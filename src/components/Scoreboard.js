14e3e8d6572e4e123b9dd8a971b98e0e2a8fd0d8
ddewe <ddewee92@gmail.com>
2021-06-03 14:22:34 +0200

    Working on scoreboard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,33 +1,85 @@
 import React, {Component} from "react";
 import FooterContent from "./FooterContent";
-import HasStartedGame, {hasStartedGame} from "../repositories/HasStartedGame";
+import HasStartedGame, {hasStartedGame} from "../services/GameInfo";
+import GameInfo from "../services/GameInfo";
+import GameProgress from "../services/GameProgress";
+import TeamAndPlayers from "../services/TeamAndPlayers";
+import MapPointers from "../services/MapPointers";
 
 
 class Scoreboard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            gameStatus: []
+            gameStatus: [],
+            gameMissions: [],
+            gameTeams: [],
+            gameProgress: [],
+            htmlRender: ''
         }
     }
 
     getGameStatus(userid) {
-        HasStartedGame.GameStarted(userid)
+        GameInfo.GameStarted(userid)
             .then(response => {
                 this.setState({
                     gameStatus: response.data
                 })
-                console.log(this.state.gameStatus)
             })
+    }
+
+    getGameScore(gameId) {
+        GameProgress.getSingleGameProgress(gameId)
+            .then(resp => {
+                this.setState({
+                    gameProgress: resp.data
+                })
+            })
+        TeamAndPlayers.getAllTeamsForGame(gameId)
+            .then(resp => {
+                this.setState({
+                    gameTeams: resp.data
+                })
+            })
+        MapPointers.getAllPointersForGame(gameId)
+            .then(resp => {
+                this.setState({
+                    gameMissions: resp.data
+                })
+            })
+    }
+
+    mapGameScore() {
+        let htmlRender = ''
+        this.state.gameMissions.map((mission) => {
+                let currentMissionId = mission.missionId.id
+                let currentMissionName = mission.missionId.missionName
+                let currentMissionScore =mission.missionId.winnerScore
+                this.state.gameProgress.map((progress) => {
+                    console.log(progress.missionid)
+                        if (progress.missionid === currentMissionId) {
+                            htmlRender += currentMissionName + " " + currentMissionScore
+                            console.log(htmlRender)
+                        }
+                        this.setState({
+                            htmlRender: htmlRender
+                        })
+                    }
+                )
+
+            }
+        )
     }
 
     componentDidMount() {
         this.getGameStatus(sessionStorage.getItem('userid'))
+        this.getGameScore(1)
+        this.mapGameScore()
     }
 
     render() {
         const insertFooter = FooterContent
-        const {gameStatus} = this.state
+        const {gameStatus, htmlRender} = this.state
         return (
             <div className="container">
                 <div className="wrapper-main">
@@ -37,7 +89,7 @@ class Scoreboard extends Component {
                     ))}
                     {gameStatus.length <= 0 ? (
                         <div>You have not selected a story.</div>
-                    ) : null }
+                    ) : htmlRender}
                 </div>
                 {insertFooter}
             </div>
